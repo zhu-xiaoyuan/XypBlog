5b5b8abb3c6af63b67892faa4f69a9703346d001
zhuxiaoyuan <281618630@qq.com>
2016-09-12 21:01:07 +0800

    Home模块变量定义

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -1,4 +1,9 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+    'TMPL_PARSE_STRING'  =>array(
+        '__JS__'=>__ROOT__ . '/Public/Home/js',
+        '__CSS__'=>__ROOT__ . '/Public/Home/css',
+        '__IMG__'=>__ROOT__.'/Public/Home/js'
+    )
 );
