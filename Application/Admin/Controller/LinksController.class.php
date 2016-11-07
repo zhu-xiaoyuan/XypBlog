9757be2183e69d17e695781b31aea0e7386bc47a
zhuxiaoyuan <281618630@qq.com>
2016-11-07 22:01:14 +0800

    友情链接

--- Application/Admin/Controller/LinksController.class.php
+++ Application/Admin/Controller/LinksController.class.php
@@ -13,6 +13,8 @@ class LinksController extends BaseController{
      */
 
     public function index(){
+        $data = M('links')->order('link_order')->select();
+        $this->assign('data',$data);
         $this->display();
     }
 
@@ -21,6 +23,54 @@ class LinksController extends BaseController{
      */
 
     public function addlink(){
-        $this->display();
+        if(IS_GET){
+            $id = I('get.id');
+            if($id){
+                $data = M('links')->where(array('id'=>$id))->find();
+                $this->assign('data',$data);
+            }
+            $this->display();
+        }else{
+            $data = I('post.');
+            //判断ID是否存在，存在id为更新，不存在为添加。
+            if($data['id']){
+                $rel =  M('links')->where(['id'=>$data['id']])->save($data);
+                if($rel){
+                    $msg['code'] = 0;
+                    $msg['msg'] = '更新成功!';
+                }else{
+                    $msg['code'] = 1;
+                    $msg['msg'] = '更新失败，请重试!';
+                }
+                $this->json_response($msg);
+            }else{
+                $rel =  M('links')->add($data);
+                if($rel){
+                    $msg['code'] = 0;
+                    $msg['msg'] = '添加成功!';
+                }else{
+                    $msg['code'] = 1;
+                    $msg['msg'] = '添加失败，请重试!';
+                }
+                $this->json_response($msg);
+            }
+        }
+
+    }
+
+    public function dellink()
+    {
+        $id = trim(I('post.id'));
+        if($id){
+            $rel = M('links')->delete($id);
+            if($rel){
+                $msg['code'] = 0;
+                $msg['msg'] = '删除成功!';
+            }else{
+                $msg['code'] = 1;
+                $msg['msg'] = '删除失败，请重试!';
+            }
+            $this->json_response($msg);
+        }
     }
 }
